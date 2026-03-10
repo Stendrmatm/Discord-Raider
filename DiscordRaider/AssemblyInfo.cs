47fe2021b26605ee822d51e1aebc8946078fa70b
Bennett82r <p.agi@mail.ru>
2026-03-10 22:19:07 +0400

    Upload Files

---
+++ DiscordRaider/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("81F86B42-3128-4FB1-BC5E-4A5BB240B9DA")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Discord Raider 2023")]
+[assembly: AssemblyProduct("Discord Raider By Unknown")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Discord Raider")]
+[assembly: AssemblyTitle("Discord Raider By Unknown")]
+[assembly: AssemblyFileVersion("6.11.161.8293")]
+[assembly: AssemblyVersion("6.11.161.8293")]
